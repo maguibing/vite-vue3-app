63e91d3f2dc6402a30ea3385faf55c4dde69908f
maguibing <kuse012@163.com>
2022-06-18 22:57:47 +0800

    pinia 练习

--- src/components/Pinia.vue
+++ src/components/Pinia.vue
@@ -1,14 +1,96 @@
 <template>
   <div class="pinia">
     <h1>我要使用pinia了</h1>
-    <span>{{count}}</span>
+    <span>{{count}}</span>---<span>{{computedMsg}}</span>---<span>{{gettersCount}}</span>---<span>{{otherGetters}}</span>
+    <br>
+    <button @click="increment">累加</button>
+    <button @click="store.$reset()">重置</button>
+    <button @click="changeMessage">修改</button>
+    <button @click="useAction">使用action</button>
   </div>
 </template>
 
 <script setup>
 import { useStore } from '../store'
-import { storeToRefs } from 'pinia'
+import { storeToRefs  } from 'pinia'
+import { computed, watch } from 'vue';
+// import  Pinia  from 'pinia';
 // storeTorefs 保持数据的响应式
-const {count} = storeToRefs(useStore())
+const store = useStore()
+// 返回的值需要 .value
+const { count,gettersCount,otherGetters } = storeToRefs(store)
+
+const computedMsg = computed(() => {
+    return count.value+ 10
+})
+
+// increment
+const increment = () => { 
+    // count.value++
+    store.$patch((state) => {
+        state.count += 20
+        state.message = '中文'
+        // count: count.value+=20,
+        // message:"一段小时"
+    })
+}
+
+// store.$subscribe((mutation, state) => { 
+//     console.log(mutation.type)
+//     console.log(mutation.storeId)
+//     console.log(mutation.payload)
+// })
+
+
+const changeMessage = () => { 
+    // store.$patch({
+    //     message:"修改后的值"
+    // })
+    // 直接赋值一个新对象 （但是貌似好像是添加了两个属性来的）
+    store.$state = { counter: 666, name: 'Paimon' }
+ 
+}
+
+const unsubscribe = store.$onAction(
+  ({
+    name, // action 的名字
+    store, // store 实例
+    args, // 调用这个 action 的参数
+    after, // 在这个 action 执行完毕之后，执行这个函数
+    onError, // 在这个 action 抛出异常的时候，执行这个函数
+  }) => {
+    // 记录开始的时间变量
+    const startTime = Date.now()
+    // 这将在 `store` 上的操作执行之前触发
+    console.log(`Start "${name}" with params [${args.join(', ')}].`)
+
+    // 如果 action 成功并且完全运行后，after 将触发。
+    // 它将等待任何返回的 promise
+    after((result) => {
+      console.log(
+        `Finished "${name}" after ${
+          Date.now() - startTime
+        }ms.\nResult: ${result}.`
+      )
+    })
+
+    // 如果 action 抛出或返回 Promise.reject ，onError 将触发
+    onError((error) => {
+      console.warn(
+        `Failed "${name}" after ${Date.now() - startTime}ms.\nError: ${error}.`
+      )
+    })
+  }
+)
+
+// // 手动移除订阅
+unsubscribe()
+
+
+// 使用action
+const useAction = () => { 
+    store.increment("传递的参数")
+}
+
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,24 @@
 import { defineStore } from 'pinia'
-
 // useStore 可以是 useUser、useCart 之类的任何东西
 // 第一个参数是应用程序中 store 的唯一 id
 export const useStore = defineStore('main', {
-  state: () => {
-    return {
-      count: 1,
-    }
+  state: () => ({
+    count: 1,
+  }),
+
+  getters: {
+    gettersCount: (state) => state.count + 20,
+
+    otherGetters() {
+      return this.gettersCount + 100
+    },
+  },
+
+  actions: {
+    increment(string) {
+      //   模块内的参数直接用this 就可以取到
+      //   console.log(this.state)
+      //   console.log(string, this.count, this.gettersCount, this.otherGetters)
+    },
   },
 })
